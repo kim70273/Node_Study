1f8ccbb10b954eabd86722b0eb001c8ece217c26
kim70273 <kim70273@naver.com>
2021-02-03 02:37:37 +0900

    ch_5

--- ch_3/3.1/var.js
+++
@@ -1,8 +0,0 @@
-const odd = '홀수 입니다.';
-const even = '짝수 입니다.';
-
-module.exports = {
-    odd,//odd:odd 키와 값변수 같아서 생략.
-    even,  
-};
-//모듈로 만들어서 다른 파일에서 쓸 수 있도록.
--- ch_3/error2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-setInterval(() => {
-    fs.unlink('./abcdefj.js', (err) => {
-        if(err){
-            console.log(err);
-        }
-    });
-}, 1000);
